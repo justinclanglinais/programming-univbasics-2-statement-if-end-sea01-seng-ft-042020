df5108a98f383397fa1cd26bd46265bbba442a0c
Justin Langlinais <justinclanglinais@gmail.com>
2020-03-30 20:10:36 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,5 +1,6 @@
 # Write your solution here
 time = Time.now 
 puts "#{time}"
-odd_even_test = time ? "Even!" : "Odd!" 
+time_test = time % 2
+odd_even_test = time = 0 ? "Even!" : "Odd!" 
 puts "#{odd_even_test}"
