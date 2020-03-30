7588a06675b093c35f5338430ac1bc6bf5244e9f
Justin Langlinais <justinclanglinais@gmail.com>
2020-03-30 20:16:33 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,6 @@
 # Write your solution here
 current_time = Time.now 
-puts "#{time}"
+puts "#{current_time}"
 time_test = current_time.to_i % 2
 odd_even_test = time_test = 0 ? "Even!" : "Odd!" 
 puts "#{odd_even_test}"
